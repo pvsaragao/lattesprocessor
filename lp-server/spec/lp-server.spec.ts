b1ab628511a5f5a618805cfdba622de81a32cf7f
Tiago Moraes <tiago3902@gmail.com>
2019-11-03 20:22:16 -0300

    [#15]Adicionado o teste de servidor da importacao do xml

---
+++ lp-server/spec/lp-server.spec.ts
@@ -0,0 +1,73 @@
+import request = require("request-promise");
+import { closeServer } from "../lp-server.ts";
+
+let base_url = "http://localhost:3000/";
+
+describe("O servidor", () => {
+  let server: any;
+
+  beforeAll(() => { server = require("../lp-server") });
+
+  afterAll(() => { server.closeServer() });
+
+  it("inicialmente retorna uma lista de publicações vazia", () => {
+    return request.get(base_url + "publications")
+      .then(body =>
+        expect(body).toBe("[]")
+      )
+      .catch(e =>
+        expect(e).toEqual(null)
+      );
+  })
+
+  it("ignora publicacoes duplicadas", () => {
+    let publicacao = {"json":{
+      "titulo": "Requisitos de software para modelo de cascata",
+      "pesquisadores": [
+        "Paulo Medeiros de Miranda Neto",
+        "Márcio Aquino da Silva",
+        "Arthur Henrique Aquino"
+      ]
+    }};
+
+    let respostaInicial = `{
+      "titulo": "Requisitos de software para modelo de cascata",
+      "pesquisadores": [
+        "Paulo Medeiros de Miranda Neto",
+        "Márcio Aquino da Silva",
+        "Arthur Henrique Aquino"
+      ]
+    }`;
+
+    return request.post(base_url + "publications", publicacao).then(body => {
+      expect(body).toEqual({ failure: "publicacao ja cadastrada" });
+      return request.get(base_url + "publications").then(body => expect(body).toBe(respostaInicial)).catch(e => expect(e).toEqual(null));
+    });
+  })
+
+  it("cadastra publicacoes corretamente", () => {
+    let publicacao = {"json":{
+      "titulo": "Requisitos de software para modelo de cascata",
+      "pesquisadores": [
+        "Paulo Medeiros de Miranda Neto",
+        "Márcio Aquino da Silva",
+        "Arthur Henrique Aquino"
+      ]
+    }};
+
+    let resposta = `{
+      "titulo": "Requisitos de software para modelo de cascata",
+      "pesquisadores": [
+        "Paulo Medeiros de Miranda Neto",
+        "Márcio Aquino da Silva",
+        "Arthur Henrique Aquino"
+      ]
+    }`;
+
+    return request.post(base_url + "publications", publicacao).then(body => {
+      expect(body).toEqual({ success: "publicacao cadastrada com sucesso" });
+      return request.get(base_url + "publications").then(body => expect(body).toBe(resposta)).catch(e => expect(e).toEqual(null));
+    });
+  })
+
+})
