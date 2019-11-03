75d79e4ad2b3a775dcb49efb8e07afc3cb3e7566
Tiago Moraes <tiago3902@gmail.com>
2019-11-03 12:10:37 -0300

    [FINISHES #16]Finalizada a implementação do teste de multiplos xml com sucesso

--- tests-acceptance/features/importLattes.feature
+++ tests-acceptance/features/importLattes.feature
@@ -10,3 +10,12 @@ And o arquivo “paulo_lattes.xml” tem as publicações “An extended triangu
 When eu selecionar para fazer o upload do arquivo “paulo_lattes.xml”
 Then eu vejo uma mensagem de sucesso na tela
 And o sistema tem as publicações “Requisitos de software para modelo de cascata”, “Estudo sobre Metodologias de Desenvolvimento Ágil”,  “An extended triangulation to the Marching Cubes 33 algorithm” e “An IoT sensor and scenario survey for data researchers”.
+
+Scenario: Fazer upload de múltiplos xml com sucesso
+Given eu estou na página de “importar Lattes”
+And as publicações “Requisitos de software para modelo de cascata” e “Estudo sobre Metodologias de Desenvolvimento Ágil” estão cadastradas no sistema,
+And o arquivo “a_lattes.xml” tem a publicação “An extended triangulation to the Marching Cubes 33 algorithm”
+And o arquivo “b_lattes.xml” tem a publicação “An IoT sensor and scenario survey for data researchers”
+When eu selecionar para fazer o upload dos arquivos “a_lattes.xml” e “b_lattes.xml”
+Then eu vejo uma mensagem de sucesso na tela
+And o sistema tem as publicações “Requisitos de software para modelo de cascata”, “Estudo sobre Metodologias de Desenvolvimento Ágil”,  “An extended triangulation to the Marching Cubes 33 algorithm” e “An IoT sensor and scenario survey for data researchers”.
--- tests-acceptance/stepsdefinitions/immportLattes.ts
+++ tests-acceptance/stepsdefinitions/immportLattes.ts
@@ -9,61 +9,121 @@ let getPublicacoes = async (xmlFile) => {
   let parser = new xml2js.Parser({ attrkey: "ATTR" });
   let xml_string = fs.readFileSync(xmlFile, "utf8");
 
-  parser.parseString(xml_string, function(error, result) {
-    if(error === null) {
-        // implementacao depende do formato xml a ser recebido...
-        // retornamos um array de titulos de publicacoes
+  parser.parseString(xml_string, function (error, result) {
+    if (error === null) {
+      // implementacao depende do formato xml a ser recebido...
+      // retornamos um array de titulos de publicacoes
     }
     else {
-        return([]);
+      return ([]);
     }
   });
 }
 
 defineSupportCode(function ({ Given, When, Then }) {
-  Given(/^Eu estou na tela de Importar Lattes$/, async () => {
-      await browser.get("http://localhost:4200/");
-      await expect(browser.getTitle()).to.eventually.equal('LattesProcessor');
-      await $("a[name='importar-lattes']").click();
+  Given(/^eu estou na tela de Importar Lattes$/, async () => {
+    await browser.get("http://localhost:4200/");
+    await expect(browser.getTitle()).to.eventually.equal('LattesProcessor');
+    await $("a[name='importar-lattes']").click();
   })
 
-  Given(/^As publicações "([^\"]*)" e "([^\"]*)" estão cadastradas no sistema$/, async (p1, p2) => {
-      var allpublicacoes : ElementArrayFinder = element.all(by.name('publicacoslist'));
-      await allpublicacoes;
-
-      var publi1 = allpublicacoes.filter(elem =>
-        elem.getText().then(text => text === p1));
-      await publi1;
-      await publi1.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-      
-      var publi2 = allpublicacoes.filter(elem =>
-        elem.getText().then(text => text === p2));
-      await publi2;
-      await publi2.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-  });
-
-  Given(/^O arquivo "([^\"]*)" tem as publicações "([^\"]*)" e "([^\"]*)"$/, async (xmlFile, p1, p2) => {
-      let publicacoesArray = getPublicacoes(xmlFile);
-      await publicacoesArray;
-
-      await expect(publicacoesArray[0].to.eventually.equal(p1));
-      await expect(publicacoesArray[1].to.eventually.equal(p2));
-      
-  });
-
-  When(/^Eu selecionar para fazer o upload do arquivo "([^\"]*)"$/, async (fileName) => {
-      await $("input[name='file-path']").sendKeys(<string> fileName);
-  });
-
-  Then(/^O sistema tem as publicações "([^\"]*)", "([^\"]*)", "([^\"]*)" e "([^\"]*)"$/, async (p1, p2, p3, p4) => {
-    var allpublicacoes : ElementArrayFinder = element.all(by.name('publicacoslist'));
+  Given(/^as publicações "([^\"]*)" e "([^\"]*)" estão cadastradas no sistema$/, async (p1, p2) => {
+    var allpublicacoes: ElementArrayFinder = element.all(by.name('publicacoslist'));
     await allpublicacoes;
 
     var publi1 = allpublicacoes.filter(elem =>
       elem.getText().then(text => text === p1));
     await publi1;
     await publi1.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-    
+
+    var publi2 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p2));
+    await publi2;
+    await publi2.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+  });
+
+  Given(/^o arquivo "([^\"]*)" tem as publicações "([^\"]*)" e "([^\"]*)"$/, async (xmlFile, p1, p2) => {
+    let publicacoesArray = getPublicacoes(xmlFile);
+    await publicacoesArray;
+
+    await expect(publicacoesArray[0].to.eventually.equal(p1));
+    await expect(publicacoesArray[1].to.eventually.equal(p2));
+
+  });
+
+  When(/^Eu selecionar para fazer o upload do arquivo "([^\"]*)"$/, async (fileName) => {
+    await $("input[name='file-path']").sendKeys(<string>fileName);
+  });
+
+  Then(/^o sistema tem as publicações "([^\"]*)", "([^\"]*)", "([^\"]*)" e "([^\"]*)"$/, async (p1, p2, p3, p4) => {
+    var allpublicacoes: ElementArrayFinder = element.all(by.name('publicacoslist'));
+    await allpublicacoes;
+
+    var publi1 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p1));
+    await publi1;
+    await publi1.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+    var publi2 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p2));
+    await publi2;
+    await publi2.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+    var publi3 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p3));
+    await publi3;
+    await publi3.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+    var publi4 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p4));
+    await publi4;
+    await publi4.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+  });
+
+  // Segundo cenario
+  Given(/^as publicações "([^\"]*)" e "([^\"]*)" estão cadastradas no sistema$/, async (p1, p2) => {
+    var allpublicacoes: ElementArrayFinder = element.all(by.name('publicacoslist'));
+    await allpublicacoes;
+
+    var publi1 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p1));
+    await publi1;
+    await publi1.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
+    var publi2 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p2));
+    await publi2;
+    await publi2.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+  });
+
+  Given(/^o arquivo "([^\"]*)" tem a publicação "([^\"]*)"$/, async (xmlFile, p) => {
+    let publicacoesArray = getPublicacoes(xmlFile);
+    await publicacoesArray;
+
+    await expect(publicacoesArray[0].to.eventually.equal(p));
+  });
+
+  Given(/^o arquivo "([^\"]*)" tem a publicação "([^\"]*)"$/, async (xmlFile, p) => {
+    let publicacoesArray = getPublicacoes(xmlFile);
+    await publicacoesArray;
+
+    await expect(publicacoesArray[0].to.eventually.equal(p));
+  });
+
+  When(/^eu selecionar para fazer o upload dos arquivos "([^\"]*)" e "([^\"]*)"$/, async (fileName1, fileName2) => {
+    let stringPaths = `${fileName1}, ${fileName2}`;
+    await $("input[name='multiple-file-paths']").sendKeys(<string>stringPaths);
+  });
+
+  Then(/^o sistema tem as publicações "([^\"]*)", "([^\"]*)", "([^\"]*)" e "([^\"]*)"$/, async (p1, p2, p3, p4) => {
+    var allpublicacoes: ElementArrayFinder = element.all(by.name('publicacoslist'));
+    await allpublicacoes;
+
+    var publi1 = allpublicacoes.filter(elem =>
+      elem.getText().then(text => text === p1));
+    await publi1;
+    await publi1.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+
     var publi2 = allpublicacoes.filter(elem =>
       elem.getText().then(text => text === p2));
     await publi2;
