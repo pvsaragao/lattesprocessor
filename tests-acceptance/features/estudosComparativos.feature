3a49ef81bcc77a2543fbd43a80a200407d1ce913
mrbelf <mbmt@cin.ufpe.br>
2019-11-03 22:36:16 -0300

    [#20]Teste de ranqueamento com mesmo número de artigos

--- tests-acceptance/features/estudosComparativos.feature
+++ tests-acceptance/features/estudosComparativos.feature
@@ -3,10 +3,17 @@ As a: Pesquisador de um grupo de pesquisa
 I want to: Comparar os pesquisadores e instutuições
 So that: Seja possível realizar um ranqueamento dos pesquisadores, departamentos e grupos de pesquisa, baseado em suas publicações.
 
-Cenário:Ranqueamento sem arquivo XML carregado
+Scenario:Ranqueamento sem arquivo XML carregado
 
 Given estou na página de “estudos comparativos”
 And o atributo “arquivo xml” está como “nenhum arquivo carregado”
 When eu seleciono a opção “quantidade de artigos”
 Then eu vejo uma mensagem informando que nenhum arquivo .xml foi carregado
+
+Scenario:Ranqueamento com mesmo número de artigos
+
+Given estou na página de “estudos comparativos”
+And “2” arquivos .xml contendo “Beatriz” com “2” artigos e  “Alberto” com “2” artigos
+When eu seleciono a opção “quantidade de artigos”
+	Then eu vejo uma tabela de ranking, onde “Alberto” com “2" artigos está acima de “Beatriz” com “2” artigos
 
--- tests-acceptance/stepdefinitions/estudosComparativos.ts
+++ tests-acceptance/stepdefinitions/estudosComparativos.ts
@@ -30,5 +30,36 @@ defineSupportCode(function ({ Given, And, When, Then }) {
                        '{"failure":"Nenhum arquivo .xml foi carregado"}'));
     });
     
+    Given(/^estou na página de “estudos comparativos”$/, async () => {
+        await browser.get("http://localhost:4200/");
+        await expect(browser.getTitle()).to.eventually.equal('LattesProcessor');
+        await $("a[name='estudosComparativos']").click();
+    });
+    
+    
+    //não faz sentido considerar o 2 como variavel se sempre vai ter só 2 nomes em seguida...
+    //considerando que qtd1 = qtd2
+    Given(/^"2" arquivos .xml contendo "([^\"]*)" com "(\d*)" artigos e "([^\"]*)" com "(\d*)" artigos$/, async (prof1,qtd1,prof2,qtd2) => {
+        var status_xml: ElementArrayFinder = element.all(by.name('status_xml'));
+        await expect(status_xml[0].text).to.eventually.equal('2 arquivos carregados');
+        var professor_list: ElementArrayFinder = element.all(by.name('professores'));
+        await professor_list.filter(elem => pAND(mesmoNome(elem, prof1), mesmaQtd(elem, qtd1))).then
+                    (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1))
+        await professor_list.filter(elem => pAND(mesmoNome(elem, prof2), mesmaQtd(elem, qtd2))).then
+                    (elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1))
+    });
+    
+    When(/^eu seleciono a opção “quantidade de artigos”$/, async () => {
+        await $("a[name='quantidade_de_artigos']").click();
+    });
+
+    Then(/^eu vejo uma tabela de ranking, onde "([^\"]*)" com "(\d*)" artigos está acima de "([^\"]*)" com "(\d*)" artigos$/, async (prof1,qtd1,prof2,qtd2) => {
+        var rank_list: ElementArrayFinder = element.all(by.name('rank_list'));
+        await expect(Promise.resolve(rank_list[0].nome)).to.eventually.equal(prof1)
+        await expect(Promise.resolve(rank_list[1].nome)).to.eventually.equal(prof2)
+        await expect(Promise.resolve(rank_list[0].qtd)).to.eventually.equal(qtd1)
+        await expect(Promise.resolve(rank_list[1].qtd)).to.eventually.equal(qtd2)
+        
+    });
 
 }) 
