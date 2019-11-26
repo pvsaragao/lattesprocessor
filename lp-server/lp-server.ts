50d453edce80cab34c1a4ea0768fec62feb6acfc
Tiago Moraes <tiago3902@gmail.com>
2019-11-26 01:23:55 -0300

    Adicionada base para o post

--- lp-server/lp-server.ts
+++ lp-server/lp-server.ts
@@ -1,17 +1,23 @@
 import express = require('express');
 import bodyParser = require("body-parser");
 
+const multer = require('multer');
+const upload = multer({ dest: '/uploads' });
+
+import { CadastroDePesquisadores } from './cadastrodepesquisadores';
+
+
 // add imports here
 
 var lpserver = express();
 
 // add services here
 
-var allowCrossDomain = function(req: any, res: any, next: any) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+var allowCrossDomain = function (req: any, res: any, next: any) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 }
 
 lpserver.use(allowCrossDomain);
@@ -19,6 +25,11 @@ lpserver.use(allowCrossDomain);
 lpserver.use(bodyParser.json());
 
 // add reqs here
+
+lpserver.post('/pesquisador/adicionar', upload.array('lattesFiles', 12), (req: express.Request, res: express.Response) => {
+  console.log(req.files);
+  // files can be seen under req.files
+})
 
 var server = lpserver.listen(3000, function () {
   console.log('Example app listening on port 3000!')
