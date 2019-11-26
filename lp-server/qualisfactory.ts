5e916a50bc2d47bb19595ec1302d895ea4e91c6a
Vinicius Dantas Januario <vdj@ug4c44.windows.cin.ufpe.br>
2019-11-26 11:52:41 -0300

    [#9]Leitura de JSON da qualis factory

--- lp-server/qualisfactory.ts
+++ lp-server/qualisfactory.ts
@@ -1,17 +1,23 @@
 import  {Qualis} from '../common/qualis';
-import {utils} from 'xlsx';
+import * as XLSX from 'xlsx';
 
 export class QualisFactory {
     // add atributes here
     qualisTable : Qualis;
-    fileContent : JSON;
+    fileContent : JSON = null;
     // add methods here
     readXls(file : File) {
-
+        let workbook : XLSX.WorkBook = XLSX.read(file);
+        let worksheet : XLSX.WorkSheet = workbook.Sheets[workbook.SheetNames[0]];
+        this.fileContent = JSON.parse(JSON.stringify(XLSX.utils.sheet_to_json(worksheet)));
     }
     
     makeQualis() {
         
+    }
+
+    getFileContent() : JSON {
+        return this.fileContent;
     }
 
     getQualis() : Qualis {
