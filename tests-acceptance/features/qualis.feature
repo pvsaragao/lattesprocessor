d47d4401b7c88b22b94dbaa40efae7b3479c26d0
Vinicius Dantas <vdj@cin.ufpe.br>
2019-12-06 22:21:37 -0300

    Ajustes finais para os cenários e testes de aceitação, com remoção de passos desnescessários

--- tests-acceptance/features/qualis.feature
+++ tests-acceptance/features/qualis.feature
@@ -6,27 +6,25 @@ So that: Eu consigo usar o critério qualis de avaliação para o rankeamento do
 Scenario: Importar planilha com sucesso
 Given Eu estou na página Qualis
 And Nenhum periódico está armazenado no sistema
-When Eu seleciono o arquivo "publicações_2016.xls"
-And Eu seleciono a opção "Qualis Import"
+When Eu seleciono o arquivo "publicacoes_2016.xls"
+And Eu seleciono a opção "Qualis Import" que carrega o arquivo
 Then Eu vejo uma mensagem de sucesso
 And Eu vejo os periódicos "ACM Transactions on Intelligent Systems and Technology" com ISSN "2157-6904" e avaliação "A1", "Applied Intelligence (Boston)" com ISSN "0924-669X" e avaliação "B1" e "BMC Genomics" com ISSN "1471-2164" e avaliação "B2"
 
 Scenario: Limpar a tabela qualis com sucesso
 Given Eu estou na página Qualis
-And Eu vejo o periódico "ACM Transactions on Intelligent Systems and Technology" com ISSN "2157-6904" e avaliação "A1", "Applied Intelligence (Boston)" com ISSN "0924-669X" e avaliação "B1" e "BMC Genomics" com ISSN "1471-2164" e avaliação "B2"
-When Eu seleciono a opção "Clear Qualis"
+And Eu consigo ver os periódicos "ACM Transactions on Intelligent Systems and Technology" com ISSN "2157-6904" e avaliação "A1", "Applied Intelligence (Boston)" com ISSN "0924-669X" e avaliação "B1" e "BMC Genomics" com ISSN "1471-2164" e avaliação "B2"
+When Eu seleciono a opção "Clear Qualis" que limpa a tabela atual
 Then Eu vejo nenhum periódico cadastrado no sistema
 
 Scenario: Importar planilha com formáto inválido
 Given Eu estou na página Qualis
 And Nenhum periódico está armazenado no sistema
-When Eu seleciono o arquivo "publicações_2018.pdf"
-And Eu seleciono a opção "Qualis Import"
+When Eu envio o arquivo "publicacoes_2018.xls", que possui uma formatação invalida
 Then Eu vejo uma mensagem de erro indicando formatação errada na planilha
 
 Scenario: Importar planilha vazia
 Given Eu estou na página Qualis
 And Nenhum periódico está armazenado no sistema
-When Eu seleciono o arquivo "publicações_2018.xls"
-And Eu seleciono a opção "Qualis Import"
+When Eu envio o arquivo "publicacoes_2019.xls", que não possui nenhuma entrada para periódico
 Then Eu vejo uma mensagem de erro indicando que nenhum periódico foi cadastrado no sistema
--- tests-acceptance/stepdefinitions/qualis.ts
+++ tests-acceptance/stepdefinitions/qualis.ts
@@ -4,6 +4,7 @@ import request = require("request-promise");
 
 let base_url = "http://localhost:3000/";
 
+let path = require('path');
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
@@ -25,16 +26,17 @@ defineSupportCode(function ({Given, When, Then}) {
         await qualisTable.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
     })
 
-    When(/^Eu seleciono o arquivo “([^\"]*)"$/, async (filePath : string) => {
+    When(/^Eu seleciono o arquivo "([^\"]*)"$/, async (fileName : string) => {
+        let filePath = path.join(__dirname, '/support_files/' + <string>fileName);
         await $("input[name=QualisFile]").sendKeys(filePath);
     })
 
-    When(/^Eu seleciono a opção “([^\"]*)"$/, async (option : string) => {
+    When(/^Eu seleciono a opção "([^\"]*)" que carrega o arquivo$/, async (option : string) => {
         await element(by.buttonText(option)).click();
     })
 
     Then(/^Eu vejo uma mensagem de sucesso$/, async () => {
-        await element(by.name('ImportStatus')).getText().then(msg => expect(Promise.resolve(msg)).to.eventually.equal('success'))
+        await element(by.name('ImportStatus')).getText().then(msg => expect(Promise.resolve(msg)).to.eventually.equal('planilha cadastrada com sucesso'))
     })
 
     Then(/^Eu vejo os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
@@ -47,7 +49,7 @@ defineSupportCode(function ({Given, When, Then}) {
                     .then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(3));
     })
     //Scenario: Limpar a tabela qualis com sucesso
-    Given(/^Eu vejo os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
+    Given(/^Eu consigo ver os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
                     async(periodico1, issn1, avaliacao1, periodico2, issn2, avaliacao2, periodico3, issn3, avaliacao3) => {
         let periodicoArray = [periodico1, periodico2, periodico3];
         let issnArray = [issn1, issn2, issn3];
@@ -57,7 +59,7 @@ defineSupportCode(function ({Given, When, Then}) {
                     .then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(3));
     })
 
-    When(/^Eu seleciono a opção “([^\"]*)"$/, async (option : string) => {
+    When(/^Eu seleciono a opção "([^\"]*)" que limpa a tabela atual$/, async (option : string) => {
         await element(by.buttonText(option)).click();
     })
 
@@ -65,39 +67,25 @@ defineSupportCode(function ({Given, When, Then}) {
         let qualisTable : ElementArrayFinder = element.all(by.name('qualisTable'));
         await qualisTable.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
     })
-    //Scenario: Importar planilha com extensão inválida 
-    Given(/^Nenhum periódico está armazenado no sistema$/, async () => {
-        let qualisTable : ElementArrayFinder = element.all(by.name('qualisTable'));
-        await qualisTable.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+
+    When(/^Eu envio o arquivo "([^\"]*)", que possui uma formatação invalida$/, async(fileName : string) => {
+        let filePath = path.join(__dirname, '/support_files/' + <string>fileName);
+        await $("input[name=QualisFile]").sendKeys(filePath);
+        await element(by.buttonText("Qualis Import")).click();
     })
 
-    When(/^Eu seleciono o arquivo “([^\"]*)”$/, async(filePath : string) => {
-        await $("input[name=Files]").sendKeys(filePath);
-    })
-    
-    When(/^Eu seleciono a opção “([^\"]*)"$/, async (option : string) => {
-        await element(by.buttonText(option)).click();
+    Then(/^Eu vejo uma mensagem de erro indicando formatação errada na planilha$/, async () => {
+        await element(by.name('ImportStatus')).getText().then(msg => expect(Promise.resolve(msg)).to.eventually.equal('planilha com formatacao invalida'));
     })
 
-    Then(/^Eu vejo uma mensagem de erro indicando o formato inválido do arquivo$/, async () => {
-        await element(by.name('ImportStatus')).getText().then(msg => expect(Promise.resolve(msg)).to.eventually.equal('planilha não possui formatação adequada'));
-    })
-    //Scenario: Importar planilha vazia
-    Given(/^Nenhum periódico está armazenado no sistema$/, async () => {
-        let qualisTable : ElementArrayFinder = element.all(by.name('qualisTable'));
-        await qualisTable.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    When(/^Eu envio o arquivo "([^\"]*)", que não possui nenhuma entrada para periódico$/, async(fileName : string) => {
+        let filePath = path.join(__dirname, '/support_files/' + <string>fileName);
+        await $("input[name=QualisFile]").sendKeys(filePath);
+        await element(by.buttonText("Qualis Import")).click();
     })
 
-    When(/^Eu seleciono o arquivo “([^\"]*)”$/, async(filePath : string) => {
-        await $("input[name=Files]").sendKeys(filePath);
-    })
-    
-    When(/^Eu seleciono a opção “([^\"]*)"$/, async (option : string) => {
-        await element(by.buttonText(option)).click();
-    })
-
-    Then(/^Eu vejo uma mensagem indicando que nenhum periódico foi importado ao sistema$/, async () => {
+    Then(/^Eu vejo uma mensagem de erro indicando que nenhum periódico foi cadastrado no sistema$/, async () => {
         await element(by.name('ImportStatus')).getText()
-            .then(msg => expect(Promise.resolve(msg)).to.eventually.equal('nenhum periodico importado!'));
+            .then(msg => expect(Promise.resolve(msg)).to.eventually.equal('nenhuma entrada adicionada'));
     })
 })
