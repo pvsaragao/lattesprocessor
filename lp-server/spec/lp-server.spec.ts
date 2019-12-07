44ba888436f29261c6340e8e55640334bf7c799a
Vinicius Dantas <vdj@cin.ufpe.br>
2019-12-06 22:27:54 -0300

    Problema com referência de arquivo no teste de serviço

--- lp-server/spec/lp-server.spec.ts
+++ lp-server/spec/lp-server.spec.ts
@@ -22,21 +22,26 @@ describe("O servidor", () => {
   })
 
   it("Cadastra uma planilha corretamente", () => {
-    let qualisFile : File;
+    let reader : FileReader;
+    let data : string = fs.readFileSync('./spec/test.xls', 'binary');
+    let qualisFile : File = new File([data], './spec/toSend.xls');
     let options : any = {method: 'POST', uri: (base_url + "qualis/adicionar"), file: qualisFile};
     return request(options).then(body => expect(body)
                   .toEqual({success : "planilha cadastrada com sucesso"}))
+                          .catch(e => expect(e).toBe(null));
   })
 
   it("Retorna a avaliação esperada de cada periódico", () => {
     let options : any = {method: 'GET', uri: (base_url + "qualis/avaliacao"), params: {periodico: "BMC Genomics"}};
     return request(options).then(body => expect(body)
                   .toEqual({success : "B2"}))
+                          .catch(e => expect(e).toBe(null));
   })
 
   it("Limpa a tabela", () => {
-    return request.post(base_url + "qualis/apagar").then(body => expect(body)
+    return request.delete(base_url + "qualis/apagar").then(body => expect(body)
                   .toEqual({success : "Tabela qualis apagada"}))
+                          .catch(e => expect(e).toBe(null));
   })
 
 })
