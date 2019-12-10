148136fd218ab9018c3609907fc0ee3709e52ab4
mrbelf <mbmt@cin.ufpe.br>
2019-12-10 16:20:25 -0300

    correcao inicial de verificar xls carregado

--- lp-gui/src/app/estudoscomparativos/estudoscomparativos.component.ts
+++ lp-gui/src/app/estudoscomparativos/estudoscomparativos.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Pesquisador } from '../../../../common/pesquisador';
-import { EstudosComparativosService } from './estudoscomparativos.service'
+import { EstudosComparativosService } from './estudoscomparativos.service';
+import { PesquisadorService } from '../pesquisador/pesquisador.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './estudoscomparativos.component.html',
     styleUrls: ['./estudoscomparativos.component.css']
-  })
+})
 
 
 export class estudoscomparativosComponent {
 
-    tabela: Pesquisador[] =  [];
-    xlscarregado: boolean = true;
-    pesos: any[] = [1,1,1,1,1,1,1,1];
+    tabela: Pesquisador[] = [];
+    xlscarregado: boolean = false;
+    pesos: any[] = [1, 1, 1, 1, 1, 1, 1, 1];
 
-    constructor(private ecService: EstudosComparativosService) {}
+    constructor(private ecService: EstudosComparativosService, private pqService: PesquisadorService) { }
 
     // onMove(): void {
     //     let xls: boolean = false;
@@ -29,30 +30,45 @@ export class estudoscomparativosComponent {
     //     }
     //  }
 
-     pesosInvalidos():boolean{
-         for(let i = 0; i<8;i++){
-             if(isNaN(Number(this.pesos[i]))){
-                 return true;
-             }
-         }
-         return false;
-     }
-     
+    pesosInvalidos(): boolean {
+        for (let i = 0; i < 8; i++) {
+            if (isNaN(Number(this.pesos[i]))) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-     gerarEstudo(): void{
-        if(this.pesosInvalidos()){
+    updateXlsCarregado(): void {
+        let pesqArr = [];
+        this.pqService.getPesquisadores().subscribe(
+            ps => { pesqArr = ps; },
+            msg => { alert(msg.message); }
+          );
+          console.log(pesqArr);
+          console.log(pesqArr.length);
+        if (pesqArr.length > 0) {
+            this.xlscarregado = true;
+        }else{
+            this.xlscarregado = false;
+        }
+    }
+
+    gerarEstudo(): void {
+        if (this.pesosInvalidos()) {
             alert('Pesos inválidos');
             return;
         }
+        this.updateXlsCarregado();
         if (!this.xlscarregado) {
-          alert('nenhum arquivo carregado');
-        }else{
+            alert('nenhum arquivo carregado');
+        } else {
             this.ecService.getRanking(this.pesos);
         }
-     }
+    }
 
-     setPadrao(): void{
-        this.pesos = [1,1,1,1,1,1,1];
-     }
+    setPadrao(): void {
+        this.pesos = [1, 1, 1, 1, 1, 1, 1];
+    }
 }
 
