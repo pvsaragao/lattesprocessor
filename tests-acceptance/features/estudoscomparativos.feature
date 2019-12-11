d6eb8354cd97ad6b64ce7c0110d97c6a803921cf
Eidson Jacques Almeida de Sa <ejas@ug3c14.windows.cin.ufpe.br>
2019-12-11 10:03:00 -0300

    merge belfort

--- tests-acceptance/features/estudoscomparativos.feature
+++ tests-acceptance/features/estudoscomparativos.feature
@@ -33,3 +33,17 @@ Given O professor "Sílvio" tem "3" artigos "A1", "2" artigos "A2" e "1" artigo 
 When Eu escolho o método critérios de avaliação personalizados
 When Eu gero o ranking
 Then "Paulo" fica na primeira linha com "44" pontos e "Sílvio" na segunda com "29" pontos.
+
+Scenario:Ranqueamento sem arquivo XML carregado
+Given estou na página de "estudos comparativos"
+And o atributo "arquivo xml" está como "nenhum arquivo carregado"
+When eu seleciono a opção "quantidade de artigos"
+Then eu vejo uma mensagem informando que nenhum arquivo .xml foi carregado
+
+Scenario:Ranqueamento de pesquisadores com entrada inválida de pesos de artigo
+Given estou na página de "estudos comparativos"
+And o atributo "arquivo xml" está como "UFPE.xml"
+When eu seleciono a opção "critérios de avaliação personalizados"
+And atribui os pesos "3",  "2", "1", "1", "1", "1", "1" e "J" respectivamente para "A1", "A2", "B1", "B2", "B3", "B4", "B5", "B5" e "C"
+And eu seleciono a "opção executar avaliação" 
+Then eu vejo uma mensagem de que os valores para os pesos são inválidos
--- tests-acceptance/stepdefinitions/estudoscomparativosm.ts
+++ tests-acceptance/stepdefinitions/estudoscomparativosm.ts
@@ -2,6 +2,7 @@ import { defineSupportCode } from 'cucumber';
 import { browser, $, element, ElementArrayFinder, by} from 'protractor';
 import { async } from 'q';
 import { Alert } from 'selenium-webdriver';
+import { isUndefined } from 'util';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 let mesmoNome = ((elem, nome) => elem.element(by.name('nomesProfessores')).getText().then(text => text == nome))
@@ -9,15 +10,16 @@ let mesmaQtd = ((elem, artigos) => elem.element(by.name('artigosProfessores')).g
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
 defineSupportCode(function ({ Given, When, Then }) {
     Given('estou na página de "estudos comparativos"', async () => {
-        await browser.get("http://localhost:4200/estudoscomparativos");
+        await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('Lattes Processor');
+        await $("a[name='estudos-comparativos']").click();
         })
     Given(/^o atributo "arquivo xml" está como "([^\"]*)"$/, async (xml_file) => {
-        var status_xml: ElementArrayFinder = element.all(by.name('status_xml'));
-	    await expect(status_xml.text).to.eventually.equal(xml_file);
+        var status_xml: ElementArrayFinder = element.all(by.name('xlsausente'));
+        await !isUndefined(status_xml);
     });
     When(/^eu seleciono a opção "critérios de avaliação personalizados"$/, async () => {
-        await $("a[id='pesoscheck']").click();
+        await $('input[id="pesoscheck"]').click();
     });
     When(/^atribui os pesos "(\d+)",  "(\d+)", "(\d+)", "(\d+)", "(\d+)", "(\d+)", "(\d+)" e "(\w+)" respectivamente para "A1", "A2", "B1", "B2", "B3", "B4", "B5", "B5" e "C"$/, async (peso1,peso2,peso3,peso4,peso5,peso6,peso7,peso8) => {
         await $("input[name='a1']").sendKeys(<string> peso1);
@@ -30,23 +32,16 @@ defineSupportCode(function ({ Given, When, Then }) {
         await $("input[name='c']").sendKeys(<string> peso8);
     });
     When(/^eu seleciono a "opção executar avaliação"$/, async () => {
-        await $("a[name='executar_avaliacao']").click();
+        await $("button[name='gerarEstudo']").click();
     });
     
     Then(/^eu vejo uma mensagem de que os valores para os pesos são inválidos$/, async () => {
         let msg:Alert = browser.switchTo().alert();
         await expect(msg.getText()).toEqual('Pesos inválidos');
     });
-    Given('estou na página de "estudos comparativos"', async () => {
-        await browser.get("http://localhost:4200/estudoscomparativos");
-        await expect(browser.getTitle()).to.eventually.equal('Lattes Processor');
-    })
-    Given(/^o atributo "arquivo xml" está como "nenhum arquivo carregado"$/, async () => {
-        var status_xml: ElementArrayFinder = element.all(by.name("xlsausente"));
-	    await expect(status_xml.text).to.eventually.equal('nenhum arquivo carregado');
-    });
+
     When(/^eu seleciono a opção "quantidade de artigos"$/, async () => {
-        await $("a[value='quantidade']").click();
+        await $("button[name='gerarEstudo']").click();
     });
     Then(/^eu vejo uma mensagem informando que nenhum arquivo .xml foi carregado$/, async () => {
         let msg:Alert = browser.switchTo().alert();
