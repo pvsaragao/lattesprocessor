0cc77c17aa5c961a851ef0387dc43ac690f25978
jose guilherme <jgnvs6@gmail.com>
2020-09-22 02:30:51 -0300

    criacao de cenarios em implementacao de testes

--- tests-acceptance/features/qualis.feature
+++ tests-acceptance/features/qualis.feature
@@ -3,28 +3,40 @@ As a: Pesquisador de um grupo de pesquisa
 I want: to Importar planilhas com extensão xls com avaliação qualis de publicações de periódicos
 So that: Eu consigo usar o critério qualis de avaliação para o rankeamento do número de produções acadêmicas de professores, departamentos e grupos de pesquisa
 
-Scenario: Importar planilha com sucesso
+Scenario: Importar planilha com formáto inválido
+Given Eu estou na página Qualis
+And Nenhum periódico está armazenado no sistema
+When seleciono a opção "Periodico" no campo "typeQualis"
+And insiro o valor "2016" no campo "year"
+And Eu seleciono o arquivo "publicacoes_2018.xls"
+And Eu seleciono a opção "Qualis Import" que carrega o arquivo
+Then Eu vejo uma mensagem de erro indicando formatação errada na planilha
+
+Scenario: Importar planilha de Periodicos com sucesso
 Given Eu estou na página Qualis
 And Nenhum periódico está armazenado no sistema
 When Eu seleciono o arquivo "publicacoes_2016.xls"
+And seleciono a opção "Periodico" no campo "typeQualis"
+And insiro o valor "2016" no campo "year"
 And Eu seleciono a opção "Qualis Import" que carrega o arquivo
 Then Eu vejo uma mensagem de sucesso
 And Eu vejo os periódicos "ACM Transactions on Intelligent Systems and Technology" com ISSN "2157-6904" e avaliação "A1", "Applied Intelligence (Boston)" com ISSN "0924-669X" e avaliação "B1" e "BMC Genomics" com ISSN "1471-2164" e avaliação "B2"
 
-Scenario: Limpar a tabela qualis com sucesso
+Scenario: Filtrar dados com sucesso
 Given Eu estou na página Qualis
-And Eu consigo ver os periódicos "ACM Transactions on Intelligent Systems and Technology" com ISSN "2157-6904" e avaliação "A1", "Applied Intelligence (Boston)" com ISSN "0924-669X" e avaliação "B1" e "BMC Genomics" com ISSN "1471-2164" e avaliação "B2"
-When Eu seleciono a opção "Clear Qualis" que limpa a tabela atual
-Then Eu vejo nenhum periódico cadastrado no sistema
+And Eu tenho os dados no server da planilha "publicacoes_2016.xls" do tipo "Periodico" e do ano "2016"
+And Eu tenho os dados no server da planilha "eventos_2018.xlsx" do tipo "Evento" e do ano "2018"
+And seleciono a opção "periodico" no campo "filterType"
+When seleciono a opção "2016" no campo "filterYear"
+Then Eu vejo os periódicos "ACM Transactions on Intelligent Systems and Technology" com ISSN "2157-6904" e avaliação "A1", "Applied Intelligence (Boston)" com ISSN "0924-669X" e avaliação "B1" e "BMC Genomics" com ISSN "1471-2164" e avaliação "B2"
+And Eu não vejo os periódicos "IEEE Conference on Computer Vision and Pattern Recognition" com ISSN "CVPR" e avaliação "A1", "International Conference on Service Oriented Computing" com ISSN "ICSOC" e avaliação "A3" e "Latin-American Algorithms, Graphs and Optimization Symposium" com ISSN "LAGOS" e avaliação "B3"
 
-Scenario: Importar planilha com formáto inválido
+Scenario: Importar planilha de Eventos com sucesso
 Given Eu estou na página Qualis
 And Nenhum periódico está armazenado no sistema
-When Eu envio o arquivo "publicacoes_2018.xls", que possui uma formatação invalida
-Then Eu vejo uma mensagem de erro indicando formatação errada na planilha
-
-Scenario: Importar planilha vazia
-Given Eu estou na página Qualis
-And Nenhum periódico está armazenado no sistema
-When Eu envio o arquivo "publicacoes_2019.xls", que não possui nenhuma entrada para periódico
-Then Eu vejo uma mensagem de erro indicando que nenhum periódico foi cadastrado no sistema
+When Eu seleciono o arquivo "eventos_2018.xlsx"
+And seleciono a opção "Evento" no campo "typeQualis"
+And insiro o valor "2019" no campo "year"
+And Eu seleciono a opção "Qualis Import" que carrega o arquivo
+Then Eu vejo uma mensagem de sucesso
+And Eu vejo os periódicos "IEEE Conference on Computer Vision and Pattern Recognition" com ISSN "CVPR" e avaliação "A1", "International Conference on Learning Representations" com ISSN "ICLR" e avaliação "A1" e "Neural Information Processing Systems" com ISSN "NeurIPS" e avaliação "A1"
--- tests-acceptance/stepdefinitions/qualis.ts
+++ tests-acceptance/stepdefinitions/qualis.ts
@@ -1,4 +1,5 @@
 import { defineSupportCode } from 'cucumber';
+import { readFileSync } from 'fs';
 import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 import request = require("request-promise");
 
@@ -13,8 +14,13 @@ let sameNome = ((elem, arr) => elem.element(by.name('allNomes')).getText().then(
 let sameAvaliacao = ((elem, arr) => elem.element(by.name('allAvaliacoes')).getText().then(text => arr.includes(text)));
 let pAND = ((p, q, r) => p.then(a => q.then(b => r.then(c => a && b && c))));
 
-defineSupportCode(function ({Given, When, Then}) {
-    //Scenario: Importar planilha com sucesso
+defineSupportCode(function ({Given, When, Then, After, setDefaultTimeout}) {
+  setDefaultTimeout(120000)  
+
+    After(async () => {
+      await request.delete(base_url + 'qualis/apagar/')
+    })
+
     Given(/^Eu estou na página Qualis$/, async () => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal("Lattes Processor");
@@ -26,29 +32,6 @@ defineSupportCode(function ({Given, When, Then}) {
         await qualisTable.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
     })
 
-    When(/^Eu seleciono o arquivo "([^\"]*)"$/, async (fileName : string) => {
-        let filePath = path.join(__dirname, '/support_files/' + <string>fileName);
-        await $("input[name=QualisFile]").sendKeys(filePath);
-    })
-
-    When(/^Eu seleciono a opção "([^\"]*)" que carrega o arquivo$/, async (option : string) => {
-        await element(by.buttonText(option)).click();
-    })
-
-    Then(/^Eu vejo uma mensagem de sucesso$/, async () => {
-        await element(by.name('ImportStatus')).getText().then(msg => expect(Promise.resolve(msg)).to.eventually.equal('planilha cadastrada com sucesso'))
-    })
-
-    Then(/^Eu vejo os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
-                    async(periodico1, issn1, avaliacao1, periodico2, issn2, avaliacao2, periodico3, issn3, avaliacao3) => {
-        let periodicoArray = [periodico1, periodico2, periodico3];
-        let issnArray = [issn1, issn2, issn3];
-        let avaliacaoArray = [avaliacao1, avaliacao2, avaliacao3];
-        let qualisTable : ElementArrayFinder = element.all(by.name('qualisTable'));
-        await qualisTable.filter(elem => pAND(sameNome(elem, periodicoArray), sameAvaliacao(elem, avaliacaoArray), sameIssn(elem, issnArray)))
-                    .then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(3));
-    })
-    //Scenario: Limpar a tabela qualis com sucesso
     Given(/^Eu consigo ver os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
                     async(periodico1, issn1, avaliacao1, periodico2, issn2, avaliacao2, periodico3, issn3, avaliacao3) => {
         let periodicoArray = [periodico1, periodico2, periodico3];
@@ -59,6 +42,60 @@ defineSupportCode(function ({Given, When, Then}) {
                     .then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(3));
     })
 
+    Given(/^Eu tenho os dados no server da planilha "([^\"]*)" do tipo "([^\"]*)" e do ano "([^\"]*)"$/, 
+      async (file: string, type: string, year: string) => {
+        let filePath = path.join(__dirname, '/support_files/' + <string>file);
+        await $("input[name=QualisFile]").sendKeys(filePath);
+        await $(`#typeQualis option[value="${ type }"]`).click()
+        await $("#year").clear() 
+        await $("#year").sendKeys(year)
+        await element(by.buttonText("Qualis Import")).click();
+    });
+
+    When(/^Eu seleciono o arquivo "([^\"]*)"$/, async (fileName : string) => {
+        let filePath = path.join(__dirname, '/support_files/' + <string>fileName);
+        await $("input[name=QualisFile]").sendKeys(filePath);
+    })
+
+    When(/^seleciono a opção "([^\"]*)" no campo "([^\"]*)"$/, async (type: string, field: string) => {
+      await $(`#${ field } option[value="${ type }"]`).click()
+    });
+
+    When(/^insiro o valor "([^\"]*)" no campo "([^\"]*)"$/, async (year: string, field: string) => {
+      await $("#" + field).sendKeys(year)
+    });
+
+    When(/^Eu seleciono a opção "([^\"]*)" que carrega o arquivo$/, async (option : string) => {
+        await element(by.buttonText(option)).click();
+    })
+    
+    Then(/^Eu vejo uma mensagem de sucesso$/, async () => {
+      await browser.sleep(2000)
+      await element(by.name('ImportStatus')).getText().then(msg => expect(Promise.resolve(msg)).to.eventually.equal('planilha cadastrada com sucesso'))
+    })
+
+    Then(/^Eu vejo os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
+                    async(periodico1, issn1, avaliacao1, periodico2, issn2, avaliacao2, periodico3, issn3, avaliacao3) => {
+        let periodicoArray = [periodico1, periodico2, periodico3];
+        let issnArray = [issn1, issn2, issn3];
+        let avaliacaoArray = [avaliacao1, avaliacao2, avaliacao3];
+        let qualisTable : ElementArrayFinder = element.all(by.name('qualisTable'));
+        await qualisTable.filter(elem => pAND(sameNome(elem, periodicoArray), sameAvaliacao(elem, avaliacaoArray), sameIssn(elem, issnArray)))
+              .then(elems => {
+                expect(Promise.resolve(elems.length)).to.eventually.equal(3)
+              });
+    })
+
+    Then(/^Eu não vejo os periódicos "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)", "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)" e "([^\"]*)" com ISSN "([^\"]*)" e avaliação "([^\"]*)"$/, 
+                    async(periodico1, issn1, avaliacao1, periodico2, issn2, avaliacao2, periodico3, issn3, avaliacao3) => {
+        let periodicoArray = [periodico1, periodico2, periodico3];
+        let issnArray = [issn1, issn2, issn3];
+        let avaliacaoArray = [avaliacao1, avaliacao2, avaliacao3];
+        let qualisTable : ElementArrayFinder = element.all(by.name('qualisTable'));
+        await qualisTable.filter(elem => pAND(sameNome(elem, periodicoArray), sameAvaliacao(elem, avaliacaoArray), sameIssn(elem, issnArray)))
+              .then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    })
+    
     When(/^Eu seleciono a opção "([^\"]*)" que limpa a tabela atual$/, async (option : string) => {
         await element(by.buttonText(option)).click();
     })
