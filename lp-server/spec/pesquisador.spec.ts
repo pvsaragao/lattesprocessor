446c7fb3291d74b73e168897719c838929fc2c91
pvsaragao <pvsa@cin.ufpe.br>
2020-10-13 02:43:40 -0300

    ajuste no teste da classe de pesquisadores

--- lp-server/spec/pesquisador.spec.ts
+++ lp-server/spec/pesquisador.spec.ts
@@ -26,11 +26,11 @@ describe("A classe Pesquisador", () => {
         expect(pesquisador.nome).toBe("");
         expect(pesquisador.publicacoes.length).toBe(0);
 
-        var publicacao = new Publicacao("Implementing distribution and persistence aspects with AspectJ", "ACM SIGPLAN Notices");
+        var publicacao = new Publicacao("States as Specifications", "I Simpósio Brasileiro de Linguagens de Programação (SBLP 1996)");
         pesquisador.addPublicacao(publicacao);
 
         var temp = pesquisador.publicacoes;
-        expect(temp[0].titulo).toBe("Implementing distribution and persistence aspects with AspectJ");
-        expect(temp[0].periodico).toBe("ACM SIGPLAN Notices");
+        expect(temp[0].titulo).toBe("States as Specifications");
+        expect(temp[0].periodico).toBe("I Simpósio Brasileiro de Linguagens de Programação (SBLP 1996)");
     })
 })
