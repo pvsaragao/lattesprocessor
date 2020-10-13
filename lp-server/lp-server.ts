7f979e599c006491b883d8b0ac463a8481b52fac
pvsaragao <pvsa@cin.ufpe.br>
2020-10-13 02:43:18 -0300

    adição das rotas do cadastro de pesquisadores

--- lp-server/lp-server.ts
+++ lp-server/lp-server.ts
@@ -6,14 +6,20 @@ const upload = multer({ dest: './uploads' });
 
 const fs = require('fs');
 
+import { Pesquisador } from '../common/pesquisador';
+import { CadastroDePesquisadores } from './cadastroDePesquisadores';
+import { PesquisadoresFactory } from './pesquisadoresFactory';
+
 var lpserver = express();
+var cadastroPesquisadores = new CadastroDePesquisadores();
+var pesqFactory = new PesquisadoresFactory(cadastroPesquisadores);
 
 // add services here
-var allowCrossDomain = function(req: any, res: any, next: any) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+var allowCrossDomain = function (req: any, res: any, next: any) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 }
 
 lpserver.use(allowCrossDomain);
@@ -23,6 +29,35 @@ lpserver.use(bodyParser.json());
 // ========== REQUESTS ==========
 
 // add reqs here
+
+lpserver.get('/pesquisadores/', (req: express.Request, res: express.Response) => {
+  res.send(JSON.stringify(cadastroPesquisadores.getPesquisadores()));
+});
+
+lpserver.post('/pesquisador/adicionar', upload.array('xmlFiles', 12), (req: express.Request, res: express.Response) => {
+  let error = false;
+
+  for (let i = 0; i < req.files.length; i++) {
+    let xml_string = fs.readFileSync(req.files[i].path, 'binary');
+    let p = pesqFactory.importLattes(xml_string);
+
+    if (p === null) {
+      error = true;
+    }
+  }
+
+  if (!error) {
+    res.send({
+      success: 'Arquivos foram importados com sucesso',
+    })
+    return;
+  }
+
+  res.send({
+    failure: 'Houve um erro ao importar os arquivos',
+  })
+
+});
 
 var server = lpserver.listen(3000, function () {
   console.log('Example app listening on port 3000!')
