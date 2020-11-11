009d0fa2e15cf65c3214f1a1476272bbcdadc188
luan1221 <luan.ssenaa@gmail.com>
2020-11-11 17:46:59 -0300

    comeco da implementacao do teste de aceitacao de gui da feature de grupos

---
+++ tests-acceptance/features/grupos.feature
@@ -0,0 +1,28 @@
+Scenario: Formação bem-sucedida de um grupo
+    Given eu estou na página de grupos
+    Given nao existe um grupo de "Centro de Informatica" cadastrado no sistema
+    When eu crio o grupo "Centro de Informatica"
+    Then eu posso ver o grupo "Centro de Informatica"
+
+# Scenario: Insercao bem-sucedida de usuários em um grupo
+#     Given eu estou na página de grupos
+#     And vejo o grupo “Inteligencia Artificial”
+#     And existem os alunos “Pedro”, “Lucas” e “João” no grupo
+#     When eu tento remover “Joao” do grupo “Inteligência Artificial”
+#     Then eu posso ver uma mensagem de confirmação
+#     And no grupo “Inteligência Artificial” existem apenas os alunos “Pedro” e “Lucas”
+
+# Scenario: Remoção bem-sucedida de um grupo
+#     Given eu estou na página de grupos
+#     And existe o grupo “Engenharia de Software” no sistema
+#     When eu removo o grupo “Engenharia de Software” do sistema
+#     Then eu recebo uma mensagem de confirmação
+#     And não existe mais o grupo “Engenharia de Software” no sistema
+
+# Scenario: Formação bem-sucedida de um grupo a partir outros  grupos existentes
+#     Given eu estou na página de grupos
+#     And existem os grupos “Redes”, “Arquitetura de Computadores”  e “Sistemas Operacionais” no sistema
+#     And não existe um grupo “Infra-estrutura” cadastrado no sistema
+#     When eu crio o grupo “Infra-estrutura” a partir dos grupos “Redes”, “Arquitetura de Computadores” e “Sistemas Operacionais”
+#     Then eu posso ver todos os usuários dos grupos “Redes”, “Sistemas Operacionais” e “Arquitetura de Computadores” no grupo “Infra-estrutura”
+
---
+++ tests-acceptance/stepdefinitions/grupo.ts
@@ -0,0 +1,49 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let sameName = ((elem, name) => elem.element(by.name('nomelist')).getText().then(text => text === name));
+let assertSizeEquals = async (set, n) => {
+    await set.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(n));
+}
+
+let assertElementsWithSameName = async (n, name) => {
+    let allgrupos: ElementArrayFinder = element.all(by.name('grupolist'));
+    let samenames = allgrupos.filter(elem => sameName(elem, name));
+    await assertSizeEquals(samenames, n);
+}
+
+let criarGrupo = async (nome) => {
+    await $("input[name='namebox']").sendKeys(<string> nome);
+    await element(by.buttonText('Criar Grupo')).click();
+}
+
+let adicionarPesquisadores = async (p1, p2, grupo) => {
+
+}
+
+defineSupportCode( ({ Given, When, Then }) => {
+
+    //Cenario 1:
+    Given(/^eu estou na página de grupos$/, async () => {
+        await browser.get('http://localhost:4200/');
+        await expect(browser.getTitle()).to.eventually.equal('Lattes Processor');
+        await $("a[name='grupos']").click();
+    });
+
+    Given(/^nao existe um grupo de "([^\"]*)" cadastrado no sistema$/, async (grupo) => {
+        await assertElementsWithSameName(0, grupo);
+    });
+
+    When(/^eu crio o grupo "([^\"]*)"$/, async (grupo) => {
+        await criarGrupo(grupo);
+    });
+
+    Then(/^eu posso ver o grupo "([^\"]*)"$/, async (grupo) => {
+        await assertElementsWithSameName(1, grupo);
+    });
+
+    //Cenario 2:
+
+});
